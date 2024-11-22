d8664940ef7fc33f5771863a4090383ae4be48a9
RSYR1906 <rsim9977@gmail.com>
2024-11-22 10:58:23 +0800

    first commit

---
+++ src/main/java/com/vttp5_ssf_day15l/vttp5_ssf_day15l/Vttp5SsfDay15lApplication.java
@@ -0,0 +1,13 @@
+package com.vttp5_ssf_day15l.vttp5_ssf_day15l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5SsfDay15lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5SsfDay15lApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vttp5_ssf_day15l/vttp5_ssf_day15l/Vttp5SsfDay15lApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vttp5_ssf_day15l.vttp5_ssf_day15l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5SsfDay15lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
